0d402b65f6dbc9f7c2525452b3b1ea748baa0a0d
Bron Gondwana <brong@opera.com>
2012-02-20 11:23:58 +0100

    move

--- wikiformat.pl
+++ wikiformat.pl
@@ -7,19 +7,52 @@ use JSON::XS;
 use File::Slurp::Unicode;
 
 my $datafile = shift || "data.json";
+my $wikifile = shift || "wikidata.txt";
 
 my $data = {};
 if (-f $datafile) {
   $data = decode_json(read_file($datafile));
 }
 
-print "||||||'''RFC List'''||\n";
-print "||RFC||Problem Solved||Plan for new Protocol||\n";
+my $wikidata = {};
+if (-f $wikifile) {
+    $wikidata = read_wiki($wikifile);
+}
+
+print <<EOF;
+= RFC List =
+
+Every RFC is the work of someone who felt enough pain from the
+lack of something that they would make the effort to write the
+document.  These problems need to be solved somehow - maybe
+inherently in a different protocol, maybe explicitly.
+
+||||||'''RFC List'''||
+||RFC||Problem Solved||Plan for new Protocol||
+EOF
 
 foreach my $num (sort { $a <=> $b } keys %$data) {
-    print "||[[http://tools.ietf.org/html/rfc$num|RFC $num]] $data->{$num}||||||\n";
+    my $url = "http://tools.ietf.org/html/rfc$num";
+    my $title = $data->{$num};
+    my $desc = $wikidata->{$num}{desc} || '';
+    my $res = $wikidata->{$num}{res} || '';
+    print "||[[$url|RFC $num]] $title||$desc||$res||\n";
 }
 
 print "\n";
-print "(auto-generated from the list stored at [[http://github.com/brong/New-Mail-Protocol/|Github]])\n";
+print "(auto-generated from the list stored at " .
+      "[[http://github.com/brong/New-Mail-Protocol/|Github]])\n";
 
+exit 0;
+
+sub read_wiki {
+    my $file = shift;
+    open(FH, "<$file") || return {};
+    while (<FH>) {
+	chomp;
+	my @bits = split "||", $_;
+	use Data::Dumper;
+	print Dumper (\@bits);
+    };
+    close(FH);
+}
